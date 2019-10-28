7b6aa4604a08cb3948970059d96518f590e1f6af
YanJiun <jiun113241@gmail.com>
2019-10-28 13:55:46 +0800

    Orgnizing code

--- shortestPath.py
+++ shortestPath.py
@@ -1,35 +1,66 @@
 import numpy as np
 
-with open("graph.txt") as f:
-    n = int(f.readline().split()[0])
-    M = int(f.readline().split()[0])
-    t = f.readline().split()
-    g = np.empty(n*n).reshape(n,n)
-    for i in range(n):
-        l = f.readline().split()
-        for j in range(n):
-            g[i][j] = float(l[j])
-    for k in range(n):
-        for V in range(n):
-            for U in range(n):
-                tmp = g[U][k] + g[k][V]
-                if g[U][V] > tmp :
-                    g[U][V] = tmp
-    Suv = g.copy()
-    print("S'uv")
-    print(Suv)
-    W = np.ceil(g/M)
-    print("W(U,V)")
-    print(W)
+class retime:
+    def __init__(self, fn):
+        try:
+            self._Pr("Path", fn)
+            with open("graph.txt") as f:
+                self._n = int(f.readline().split()[0])
+                self._M = int(f.readline().split()[0])
+                t = f.readline().split()
+                self._t = np.empty(self._n)
+                for i in range(self._n):
+                    self._t[i] = float(t[i])
+                n = self._n
+                self.g = np.empty(n*n).reshape(n,n)
+                for i in range(self._n):
+                    l = f.readline().split()
+                    for j in range(self._n):
+                        self.g[i][j] = float(l[j])
+                self._g = self.g.copy()
+            self._Pr("Graph", self.g)
+        except:
+            print("Can't open file.")
+            raise("Load file error.")
 
-    #t = [1, 1, 2, 2, 0]
-    for row in range(n):
-        for col in range(n):
-            if col == row :
-                g[row][col] = float(t[col])
-            else:
-                tmp = W[row][col]
-                g[row][col] = M*tmp - Suv[row][col] + float(t[col])
-    D = g
-    print("D(U,V)")
-    print(D)
+    def Run(self):
+        self.Suv()
+        self.W()
+        self.D()
+
+    def _Pr(self, strIn, obj):
+        print(strIn + "=")
+        print(obj)
+        print()
+
+    def Suv(self):
+        for k in range(self._n):
+            for V in range(self._n):
+                for U in range(self._n):
+                    tmp = self.g[U][k] + self.g[k][V]
+                    if self.g[U][V] > tmp :
+                        self.g[U][V] = tmp
+        self.Suv = self.g.copy()
+        self._Pr("S'uv", self.Suv)
+
+    def W(self):
+        self.W = np.ceil(self.g/self._M)
+        self._Pr("W(U,V)", self.W)
+
+    def D(self):
+        for row in range(self._n):
+            for col in range(self._n):
+                if col == row :
+                    self.g[row][col] = self._t[col]
+                else:
+                    tmp = self.W[row][col]
+                    self.g[row][col] = self._M*tmp - self.Suv[row][col] + float(self._t[col])
+        self.D = self.g
+        self._Pr("D(U,V)", self.D)
+
+    def TimeConstrain(self, c):
+
+
+if __name__ == "__main__" :
+    app = retime("/home/yanjiun/Desktop/archIC/graph.txt")
+    app.Run()
