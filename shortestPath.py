38d1cc7f679b0ddaf0506df177f8333b6e68fffa
YanJiun <jiun113241@gmail.com>
2019-10-28 20:37:40 +0800

    Add Retiming time constrains graph

--- shortestPath.py
+++ shortestPath.py
@@ -4,63 +4,94 @@ class retime:
     def __init__(self, fn):
         try:
             self._Pr("Path", fn)
-            with open("graph.txt") as f:
+            with open(fn) as f:
                 self._n = int(f.readline().split()[0])
-                self._M = int(f.readline().split()[0])
-                t = f.readline().split()
-                self._t = np.empty(self._n)
-                for i in range(self._n):
-                    self._t[i] = float(t[i])
                 n = self._n
-                self.g = np.empty(n*n).reshape(n,n)
-                for i in range(self._n):
-                    l = f.readline().split()
-                    for j in range(self._n):
-                        self.g[i][j] = float(l[j])
-                self._g = self.g.copy()
-            self._Pr("Graph", self.g)
+                self._t = np.array(f.readline().split(), dtype="float")
+                self._M = n * self._t.max()
+                self._g = np.empty(n*n).reshape(n,n)
+                for i in range(n):
+                    self._g[i] = np.array(f.readline().split(), dtype="float")
+                self.g = self._M * self._g
+                for i in range(n):
+                    self.g[i] = self.g[i] - self._t[i]
+            self._Pr("G",self._g)
+            self._Pr("G\'", self.g)
         except:
             print("Can't open file.")
             raise("Load file error.")
 
-    def Run(self):
-        self.Suv()
-        self.W()
-        self.D()
+    def Init(self):
+        self.CalSuv()
+        self.CalW()
+        self.CalD()
 
     def _Pr(self, strIn, obj):
         print(strIn + "=")
         print(obj)
         print()
 
-    def Suv(self):
-        for k in range(self._n):
-            for V in range(self._n):
-                for U in range(self._n):
-                    tmp = self.g[U][k] + self.g[k][V]
-                    if self.g[U][V] > tmp :
-                        self.g[U][V] = tmp
+    def CalG_(self):
+        self.g_ = self.g * self._M
+
+    def _ShortestPath(self, g):
+        n = g.shape[0]
+        for k in range(n):
+            for V in range(n):
+                for U in range(n):
+                    tmp = g[U][k] + g[k][V]
+                    if g[U][V] > tmp :
+                        g[U][V] = tmp
+        for i in range(n):
+            if g[i][i] < 0 :
+                raise("Shortest path has negtive cycles.")
+        return g
+
+    def CalSuv(self):
+        self.g = self._ShortestPath(self.g)
         self.Suv = self.g.copy()
         self._Pr("S'uv", self.Suv)
 
-    def W(self):
+    def CalW(self):
         self.W = np.ceil(self.g/self._M)
+        for i in range(self._n):
+            self.W[i][i] = 0
         self._Pr("W(U,V)", self.W)
 
-    def D(self):
+    def CalD(self):
         for row in range(self._n):
             for col in range(self._n):
                 if col == row :
                     self.g[row][col] = self._t[col]
                 else:
                     tmp = self.W[row][col]
-                    self.g[row][col] = self._M*tmp - self.Suv[row][col] + float(self._t[col])
+                    self.g[row][col] = self._M*tmp - self.Suv[row][col] + self._t[col]
         self.D = self.g
         self._Pr("D(U,V)", self.D)
 
-    def TimeConstrain(self, c):
+    def CalTimeConstrain(self, c):
+        self.Cg = self._g.copy()
+        c = self.D > c
+        tmp = np.minimum(self.Cg, self.W - 1)
+        print(tmp)
+        print(c)
+        for U in range(self._n):
+            for V in range(self._n):
+                if c[U][V] :
+                    self.Cg[U][V] = tmp[U][V]
+        self.Cg = self.Cg.transpose()
+        exNode = np.zeros(self._n)
+        self.Cg = np.vstack((self.Cg, exNode))
+        exNode = np.array([np.inf]*(self._n+1)).reshape((self._n+1),1)
+        self.Cg = np.hstack((self.Cg,exNode))
+        self._Pr("Constrains graph", self.Cg)
 
+    def Retiming(self):
+        self.Result = self._ShortestPath(self.Cg)
+        self._Pr("Result", self.Result)
 
 if __name__ == "__main__" :
-    app = retime("/home/yanjiun/Desktop/archIC/graph.txt")
-    app.Run()
+    app = retime("graph.txt")
+    app.Init()
+    app.CalTimeConstrain(2)
+    app.Retiming()
